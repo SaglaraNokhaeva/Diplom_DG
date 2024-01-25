43b8f2bb0557a71e7371b3d2f529067f102f0cb6
Saglara <nohaevass@yandex.ru>
2024-01-25 22:42:55 +0300

    1

--- test.py
+++ test.py
@@ -3,41 +3,27 @@ from kivymd.app import MDApp
 from kivymd.uix.pickers import MDTimePicker, MDDatePicker
 
 
-# from kivymd.uix.picker import MDDatePicker, MDTimePicker
-
-
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Light"
         self.theme_cls.primary_palette = "BlueGray"
         return Builder.load_file('test.kv')
 
+        # Click OK
+    def on_save(self, instance, value, date_range):
+        # print(instance, value, date_range)
+        self.root.ids.date_label.text = str(value)
+
+        # Click Cancel
+    def on_cancel(self, instance, value):
+        self.root.ids.date_label.text = "Clicked Cancel"
+
+    # Get Date
     def show_date_picker(self):
         date_dialog = MDDatePicker(2023,9,1)
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
         date_dialog.open()
 
-
-
-
-    # Get Time
-    # def get_time(self, instance, time):
-    #     self.root.ids.time_label.text = str(time)
-    #
-    # # Cancel
-    # def on_cancel(self, instance, time):
-    #     self.root.ids.time_label.text = "You Clicked Cancel!"
-    #
-    # def show_time_picker(self):
-    #     from datetime import datetime
-    #
-    #     # Define default time
-    #     default_time = datetime.strptime("4:20:00", '%H:%M:%S').time()
-    #
-    #     time_dialog = MDTimePicker()
-    #     # Set default Time
-    #     time_dialog.set_time(default_time)
-    #     time_dialog.bind(on_cancel=self.on_cancel, time=self.get_time)
-    #     time_dialog.open()
 
 
 MainApp().run()
