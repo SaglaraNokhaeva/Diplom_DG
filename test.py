e07fd99b4426d1e435f3a55ce2b523cb711a99b1
Saglara <nohaevass@yandex.ru>
2024-01-30 17:55:23 +0300

    1

--- test.py
+++ test.py
@@ -6,7 +6,28 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 kv = 'test.kv'
 
 class StudyPeriodsWindow(Screen):
-    pass
+    def on_save(self, instance, value, date_range):
+        # print(instance, value, date_range)
+        # self.root.ids.date_label.text = str(value)
+        self.root.ids.date_label1.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+        self.root.ids.date_label2.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+        self.root.ids.date_label3.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+        self.root.ids.date_label4.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+
+        # Click Cancel
+
+    def on_cancel(self, instance, value):
+        self.root.ids.date_label1.text = "Clicked Cancel"
+        self.root.ids.date_label2.text = "Clicked Cancel"
+        self.root.ids.date_label3.text = "Clicked Cancel"
+        self.root.ids.date_label4.text = "Clicked Cancel"
+
+    # Get Date
+    def show_date_picker(self):
+        # date_dialog = MDDatePicker(2023,9,1)
+        date_dialog = MDDatePicker(mode='range')
+        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+        date_dialog.open()
 
 class WeekdaysWindow(Screen):
     pass
@@ -31,28 +52,7 @@ class MainApp(MDApp):
         return self.screen
 
         # Click OK
-    def on_save(self, instance, value, date_range):
-        # print(instance, value, date_range)
-        # self.root.ids.date_label.text = str(value)
-        self.root.ids.date_label1.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-        self.root.ids.date_label2.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-        self.root.ids.date_label3.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-        self.root.ids.date_label4.text = f'{str(date_range[0])} - {str(date_range[-1])}'
 
-
-        # Click Cancel
-    def on_cancel(self, instance, value):
-        self.root.ids.date_label1.text = "Clicked Cancel"
-        self.root.ids.date_label2.text = "Clicked Cancel"
-        self.root.ids.date_label3.text = "Clicked Cancel"
-        self.root.ids.date_label4.text = "Clicked Cancel"
-
-    # Get Date
-    def show_date_picker(self):
-        # date_dialog = MDDatePicker(2023,9,1)
-        date_dialog = MDDatePicker(mode='range')
-        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
-        date_dialog.open()
 
 
 
