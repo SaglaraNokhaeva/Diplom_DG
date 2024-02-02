6df4332f6f0ee47b03ab7b42b96bacb8eefac879
Saglara <nohaevass@yandex.ru>
2024-02-03 01:39:25 +0300

    1

--- test.py
+++ test.py
@@ -7,48 +7,29 @@ from kivy.properties import ObjectProperty, partial
 kv = 'test.kv'
 
 class StudyPeriodsWindow(Screen):
+    btn = ObjectProperty(None)
 
-    date_label1 = ObjectProperty(None)
-    date_label2 = ObjectProperty(None)
-    date_label3 = ObjectProperty(None)
-    date_label4 = ObjectProperty(None)
+    date1 = ObjectProperty(None)
+    date2 = ObjectProperty(None)
+    date3 = ObjectProperty(None)
+    date4 = ObjectProperty(None)
 
 
-    def on_save(self, instance, value, date_range):
+    def on_save(self, x,instance, value, date_range):
+        print(x)
+        print(instance)
+        print(self.ids.write.ids.x.id)
+        match x:
+            case 'btn0':
+                self.ids.date1.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+            case 'btn1':
+                self.ids.date2.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+            case 'btn2':
+                self.ids.date3.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+            case 'btn3':
+                self.ids.date4.text = f'{str(date_range[0])} - {str(date_range[-1])}'
 
-        # print(instance, value, date_range)
-        # self.ids.date_label1.text = str(value)
-        # self.buttonsid = []
-        # for x in range(3):
-            # self.buttonsid = [self.ids.btn0, self.ids.btn1, self.ids.btn2, self.ids.btn3]
-            # print(self.ids.btn())
-            # self.buttonsid.append(self.ids.btn(x))
-            # print(self.ids.btn())
-        self.buttonsid.bind(on_release=partial(self.HoldButtonNum, x))
-        # print(instance.ids)
-        # match instance.ids:
-        #     case 'btn0':
-        #         self.ids.date_label1.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-        #     case 'btn1':
-        #         self.ids.date_label2.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-        #     case 'btn2':
-        #         self.ids.date_label3.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-        #     case 'btn3':
-        #         self.ids.date_label4.text = f'{str(date_range[0])} - {str(date_range[-1])}'
 
-        # for x in range(3):
-        #     self.buttonsid[x].bind(on_release=partial(self.HoldButtonNum, x))
-
-        #
-        # if self.MDRaisedButton.text == 'Выберите даты начала и окончания учебного года':
-
-        self.ids.date_label2.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-        self.ids.date_label3.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-        self.ids.date_label4.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-
-    def HoldButtonNum(self, x, instance):
-        print('Button instance:', instance)
-        print('Button index in list:', x)
 
         # Click Cancel
 
@@ -59,10 +40,12 @@ class StudyPeriodsWindow(Screen):
         self.root.ids.date_label4.text = "Clicked Cancel"
 
     # Get Date
-    def show_date_picker(self):
+    def show_date_picker(self, x):
         # date_dialog = MDDatePicker(2023,9,1)
         date_dialog = MDDatePicker(mode='range')
-        date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
+
+            # self.btn[i].bind(on_release=partial(self.HoldButtonNum,  i))
+        date_dialog.bind(on_save=partial(self.on_save, x), on_cancel=self.on_cancel)
         date_dialog.open()
 
 class WeekdaysWindow(Screen):
