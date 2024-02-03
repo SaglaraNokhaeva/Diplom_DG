4aa87466710bb17b53cde87aca9f94fcc2adcc00
Saglara <nohaevass@yandex.ru>
2024-02-03 10:11:35 +0300

    1

---
+++ ex.py
@@ -0,0 +1,96 @@
+import datetime as dt
+from datetime import date, timedelta
+import pandas as pd
+from collections import Counter
+import calendar
+
+# Установка дат начала и окончания учебного года
+date_str = input("Введите дату начала учебного года (dd/mm/yyyy)\n")
+beginning_of_the_year = dt.datetime.strptime(date_str, '%d/%m/%Y').date()
+date_str = input("Введите дату окончания учебного года (dd/mm/yyyy)\n")
+end_of_the_year = dt.datetime.strptime(date_str, '%d/%m/%Y').date()
+# print(beginning_of_the_year)
+# print(end_of_the_year)
+
+
+
+# Установка дат начала и окончания каникул
+vacations = []
+while True:
+    date_str_start = input("Введите дату начала каникул (dd/mm/yyyy)\n")
+    if date_str_start == '':
+        break
+    try:
+        beginning_of_the_vacations = dt.datetime.strptime(date_str_start, '%d/%m/%Y').date()
+        date_str_end = input("Введите дату окончания каникул (dd/mm/yyyy)\n")
+        end_of_the_vacations = dt.datetime.strptime(date_str_end, '%d/%m/%Y').date()
+        for i in pd.date_range(beginning_of_the_vacations, end_of_the_vacations):
+            vacations.append(i)
+    except:
+        break
+print(vacations)
+
+#Добавление праздников и перенесенных выходных дней
+
+while True:
+    date_str = input("Введите праздничный день (dd/mm/yyyy)\n")
+    if date_str == '':
+        break
+    try:
+        date = pd.Timestamp(dt.datetime.strptime(date_str, '%d/%m/%Y').date())
+        print(type(date))
+        vacations.append(date)
+    except:
+        break
+print(vacations)
+
+
+
+# Ввод дней недели, когда будут уроки
+less = []
+
+while True:
+    str = input("Введите дни недели, в которые проходят уроки через '' (Пн - '0', Вт - '1' и т.д. ): \n")
+    if str == '':
+        break
+    try:
+        less = list(map(int, str.split()))
+        lessons = Counter(less)
+    except:
+        break
+print(lessons)
+
+
+
+
+# Вывод всех дат в диапазоне
+# start_date = date(2023, 9, 1)
+# end_date = date(2023, 9, 30)
+daterange = pd.date_range(beginning_of_the_year, end_of_the_year)
+
+for single_date in daterange:
+    if single_date not in vacations:
+        for item in lessons.items():
+            if single_date.day_of_week == item[0]:
+                for j in range(item[1]):
+                    print(single_date, calendar.day_name[single_date.day_of_week])
+
+
+
+
+# def daterange(start_date, end_date):
+#     for n in range(int((end_date - start_date).days+1)):
+#         for i in range(len(vacations)):
+#             if not vacations[i][0] <= (start_date + timedelta(n)) <= vacations[i][1]:
+#                 yield start_date + timedelta(n)
+#         continue
+
+
+
+
+# start_date = date(2023, 9, 1)
+# end_date = date(2023, 9, 30)
+# delta = end_date - start_date
+# print(delta)
+# for single_date in daterange(start_date, end_date):
+#     print(single_date.strftime("%Y-%m-%d"))
--- test.py
+++ test.py
@@ -7,50 +7,90 @@ from kivy.properties import ObjectProperty, partial
 kv = 'test.kv'
 
 class StudyPeriodsWindow(Screen):
-    btn = ObjectProperty(None)
 
-    date1 = ObjectProperty(None)
-    date2 = ObjectProperty(None)
-    date3 = ObjectProperty(None)
-    date4 = ObjectProperty(None)
+    def input_study_period(self, x):
+        date_dialog = MDDatePicker(mode='range')
+        date_dialog.bind(on_save=partial(self.on_save, x), on_cancel=self.on_cancel)
+        # print(x)
+        date_dialog.open()
 
-
-    def on_save(self, x,instance, value, date_range):
-        print(x)
-        print(instance)
-        print(self.ids.write.ids.x.id)
+    def on_save(self, x, instance, value, date_range):
         match x:
             case 'btn0':
                 self.ids.date1.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+                print(date_range)
             case 'btn1':
                 self.ids.date2.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+                print(date_range)
             case 'btn2':
                 self.ids.date3.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+                print(date_range)
             case 'btn3':
                 self.ids.date4.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+                print(date_range)
+
+    def on_cancel(self, x, instance):
+        match x:
+            case 'btn0':
+                self.ids.date1.text = "Clicked Cancel"
+            case 'btn1':
+                self.ids.date2.text = "Clicked Cancel"
+            case 'btn2':
+                self.ids.date3.text = "Clicked Cancel"
+            case 'btn3':
+                self.ids.date4.text = "Clicked Cancel"
 
 
 
-        # Click Cancel
 
-    def on_cancel(self, instance, value):
-        self.root.ids.date_label1.text = "Clicked Cancel"
-        self.root.ids.date_label2.text = "Clicked Cancel"
-        self.root.ids.date_label3.text = "Clicked Cancel"
-        self.root.ids.date_label4.text = "Clicked Cancel"
 
-    # Get Date
-    def show_date_picker(self, x):
-        # date_dialog = MDDatePicker(2023,9,1)
-        date_dialog = MDDatePicker(mode='range')
 
-            # self.btn[i].bind(on_release=partial(self.HoldButtonNum,  i))
-        date_dialog.bind(on_save=partial(self.on_save, x), on_cancel=self.on_cancel)
-        date_dialog.open()
+
+
+
+    # btn = ObjectProperty(None)
+    #
+    # date1 = ObjectProperty(None)
+    # date2 = ObjectProperty(None)
+    # date3 = ObjectProperty(None)
+    # date4 = ObjectProperty(None)
+    #
+    #
+    # def on_save(self, x,instance, value, date_range):
+    #     print(x)
+    #     print(instance)
+    #     print(self.ids.write.ids.x.id)
+    #     match x:
+    #         case 'btn0':
+    #             self.ids.date1.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+    #         case 'btn1':
+    #             self.ids.date2.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+    #         case 'btn2':
+    #             self.ids.date3.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+    #         case 'btn3':
+    #             self.ids.date4.text = f'{str(date_range[0])} - {str(date_range[-1])}'
+    #
+    #
+    #
+    #     # Click Cancel
+    #
+    # def on_cancel(self, instance, value):
+    #     self.root.ids.date_label1.text = "Clicked Cancel"
+    #     self.root.ids.date_label2.text = "Clicked Cancel"
+    #     self.root.ids.date_label3.text = "Clicked Cancel"
+    #     self.root.ids.date_label4.text = "Clicked Cancel"
+    #
+    # # Get Date
+    # def show_date_picker(self, x):
+    #     # date_dialog = MDDatePicker(2023,9,1)
+    #     date_dialog = MDDatePicker(mode='range')
+    #
+    #         # self.btn[i].bind(on_release=partial(self.HoldButtonNum,  i))
+    #     date_dialog.bind(on_save=partial(self.on_save, x), on_cancel=self.on_cancel)
+    #     date_dialog.open()
 
 class WeekdaysWindow(Screen):
     pass
-
 
 class PublicHolidaysWindow(Screen):
     pass
