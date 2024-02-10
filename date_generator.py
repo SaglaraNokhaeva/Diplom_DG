ef1e0599757b0503fcd729122a06aceec03f03c3
Saglara <nohaevass@yandex.ru>
2024-02-10 17:52:07 +0300

    1

--- date_generator.py
+++ date_generator.py
@@ -15,6 +15,7 @@ school_days = []
 week_days = []
 result_school_days = []
 
+
 class StudyPeriodsWindow(Screen):
 
     def input_study_period(self, x):
@@ -29,22 +30,15 @@ class StudyPeriodsWindow(Screen):
             case 'btn0':
                 self.ids.date1.text = f'{str(date_range[0])} - {str(date_range[-1])}'
                 school_days = date_range
-                print(school_days)
             case 'btn1':
                 self.ids.date2.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-                # print(date_range)
                 school_days = [x for x in school_days if x not in date_range]
-                print(school_days)
             case 'btn2':
                 self.ids.date3.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-                # print(date_range)
                 school_days = [x for x in school_days if x not in date_range]
-                print(school_days)
             case 'btn3':
                 self.ids.date4.text = f'{str(date_range[0])} - {str(date_range[-1])}'
-                # print(date_range)
                 school_days = [x for x in school_days if x not in date_range]
-                print(school_days)
 
     def on_cancel(self, x, instance):
         pass
@@ -58,14 +52,16 @@ class StudyPeriodsWindow(Screen):
     #     date_dialog.bind(on_save=partial(self.on_save, x), on_cancel=self.on_cancel)
     #     date_dialog.open()
 
+
 class WeekdaysWindow(Screen):
 
     def input_working_days_of_the_week(self):
         global week_days
 
-        week_days = [int(self.ids.monday_count.text), int(self.ids.tuesday_count.text), int(self.ids.wednesday_count.text), int(self.ids.thursday_count.text), int(self.ids.friday_count.text), int(self.ids.saturday_count.text)]
+        week_days = [int(self.ids.monday_count.text), int(self.ids.tuesday_count.text),
+                     int(self.ids.wednesday_count.text), int(self.ids.thursday_count.text),
+                     int(self.ids.friday_count.text), int(self.ids.saturday_count.text)]
         return week_days
-
 
 
 class PublicHolidaysWindow(Screen):
@@ -73,7 +69,6 @@ class PublicHolidaysWindow(Screen):
     def input_public_holidays(self):
         date_dialog = MDDatePicker()
         date_dialog.bind(on_save=self.on_save, on_cancel=self.on_cancel)
-        # print(x)
         date_dialog.open()
 
     def on_save(self, instance, value, date_range):
@@ -81,59 +76,38 @@ class PublicHolidaysWindow(Screen):
 
         self.ids.dates.text = self.ids.dates.text + f"\n{str(value)}"
         school_days = [x for x in school_days if x not in [value]]
-        print(school_days)
+
     def on_cancel(self, instance, value):
         pass
 
 
-
-
 class ResultWindow(Screen):
-
 
     def output_of_results(self):
         global week_days
         global school_days
         global result_school_days
 
-        print('week_days')
-        print(week_days)
-
-
-# Создаём результирующий список дат и выводим текстовое поле
         for single_date in school_days:
             for i in range(len(week_days)):
                 if single_date.weekday() == i:
                     for j in range(week_days[i]):
                         self.ids.dates.text = self.ids.dates.text + f"\n{single_date.strftime('%Y-%m-%d')}"
                         result_school_days.append(single_date)
-                        print(single_date, calendar.day_name[single_date.weekday()])
 
-        print(result_school_days)
-
+        
     def save_to_file(self):
-       global result_school_days
+        global result_school_days
 
-       MyFile = open('dates.txt', 'w')
-       for element in result_school_days:
-           MyFile.write(str(element))
-           MyFile.write('\n')
-       MyFile.close()
-
-
-
+        MyFile = open('dates.txt', 'w')
+        for element in result_school_days:
+            MyFile.write(str(element))
+            MyFile.write('\n')
+        MyFile.close()
 
     def close_application(self):
         App.get_running_app().stop()
         Window.close()
-
-
-
-
-
-
-
-
 
         # for single_date in school_days:
         #     for item in lessons.items():
@@ -149,11 +123,11 @@ class ResultWindow(Screen):
 class WindowManager(ScreenManager):
     pass
 
+
 class MainApp(MDApp):
     def build(self):
-        # self.theme_cls.theme_style = "Light"
-        # self.theme_cls.primary_palette = "BlueGray"
         self.screen = Builder.load_file(kv)
         return self.screen
+
 
 MainApp().run()
